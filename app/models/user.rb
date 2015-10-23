df7c2c428d91c1285e0efe2e168fc3cca4182494
Jack Li <jackprius@gmail.com>
2015-10-23 12:46:00 -0700

    Minimum user password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,7 +88,7 @@ class User < ActiveRecord::Base
  ?:\r\n)?[ \t])*))*)?;\s*)
   validates :email, presence: true, length: { maximum: 50}, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
 
-  validates :password, presence: true, length: { maximum: 30 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 
