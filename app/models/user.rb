c7d1972650a75c8c1d3b3493f051a5b5608514b7
Jack Li <jackprius@gmail.com>
2015-10-22 22:27:44 -0700

    Added name and email uniqueness at model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
 
   VALID_EMAIL_REGEX = (?:(?:\r\n)?[ \t])*(?:(?:(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t]
  )+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:
@@ -84,7 +84,7 @@ class User < ActiveRecord::Base
  .(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z
  |(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*\>(?:(
  ?:\r\n)?[ \t])*))*)?;\s*)
-  validates :email, presence: true, length: { maximum: 50}, format: { with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, length: { maximum: 50}, format: { with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false}
 
   validates :password, presence: true, length: { maximum: 30 }
 
