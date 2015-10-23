e41bdc7bb9fd9a75d1c9f14979d2651a3ec9bc07
Jack Li <jackprius@gmail.com>
2015-10-22 22:32:02 -0700

    Ignore fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023050622) do
+ActiveRecord::Schema.define(version: 20151023052943) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,8 @@ ActiveRecord::Schema.define(version: 20151023050622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
 
 end
